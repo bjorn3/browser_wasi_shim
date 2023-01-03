e4b4f68a9c196e4eaec6c054f068caaa6137191e
Benjamin Eckel <bhelx@simst.im>
2023-01-03 17:48:47 -0600

    feat: Expose public top-level API
    This PR creates an index.js to establish a top level, public API for the
    library. It also updates the example to use the npm library.
    
    Open to exporting other stuff in the public API. I'm not yet sure what is
    meant to be exposed and what isn't.
    
    > **Note**: I bumped the version to 0.2.0 and you'll need to publish it
    for the README example to work.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import WASI from "./wasi.js"
+import { File } from "./fs_core.js"
+import { PreopenDirectory } from "./fs_fd.js"
+
+module.exports = {
+    WASI,
+    File,
+    PreopenDirectory,
+}
+
