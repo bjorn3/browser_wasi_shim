5c294350bd545aa4c99f8d2a04ec7c46cdd72647
Systemcluster <me@systemcluster.me>
2023-03-05 10:40:39 +0100

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import WASI from "./wasi";
 export { WASI };
 
-export { File } from "./fs_core";
-export { PreopenDirectory } from "./fs_fd";
+export { File, Directory } from "./fs_core";
+export { OpenFile, PreopenDirectory } from "./fs_fd";
 export { strace } from "./strace";
-
-
