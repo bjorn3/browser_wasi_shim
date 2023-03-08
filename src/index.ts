905e020eb7ca453fd4f216c025c9995a4e2ad1a4
Systemcluster <me@systemcluster.me>
2023-03-08 20:19:40 +0100

    Add missing export for Fd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import WASI from "./wasi.js";
 export { WASI };
 
+export { Fd } from './fd.js';
 export { File, Directory } from "./fs_core.js";
-export { OpenFile, PreopenDirectory } from "./fs_fd.js";
+export { OpenFile, OpenDirectory, PreopenDirectory } from "./fs_fd.js";
 export { strace } from "./strace.js";
