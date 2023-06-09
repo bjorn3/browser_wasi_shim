284a05bd62213c6d85e37aac5b29e8e8754c082b
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-06-09 17:10:44 +0200

    Print return value in strace

--- src/strace.ts
+++ src/strace.ts
@@ -2,13 +2,15 @@
 export function strace<T extends object>(imports: T, no_trace: Array<string|symbol>) {
   return new Proxy(imports, {
     get(target, prop, receiver) {
-      let res = Reflect.get(target, prop, receiver);
+      let f = Reflect.get(target, prop, receiver);
       if (no_trace.includes(prop)) {
-        return res;
+        return f;
       }
       return function (...args) {
         console.log(prop, "(", ...args, ")");
-        return Reflect.apply(res as Function, receiver, args);
+        let result = Reflect.apply(f as Function, receiver, args);
+        console.log(" =", result);
+        return result;
       };
     },
   });
