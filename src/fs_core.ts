61f5fa5d36b1bd3a8f2624080eba361b200924fc
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-12-23 20:10:06 +0100

    Fix interesting_paths test

--- src/fs_core.ts
+++ src/fs_core.ts
@@ -581,13 +581,18 @@ class Path {
     if (path.startsWith("/")) {
       return { ret: wasi.ERRNO_NOTCAPABLE, path: null };
     }
+    if (path.includes("\0")) {
+      return { ret: wasi.ERRNO_INVAL, path: null };
+    }
 
     for (const component of path.split("/")) {
       if (component === "" || component === ".") {
         continue;
       }
       if (component === "..") {
-        self.parts.pop();
+        if (self.parts.pop() == undefined) {
+          return { ret: wasi.ERRNO_NOTCAPABLE, path: null };
+        }
         continue;
       }
       self.parts.push(component);
@@ -640,6 +645,13 @@ export class Directory extends Inode {
         return { ret: wasi.ERRNO_NOENT, entry: null };
       }
     }
+
+    if (path.is_dir) {
+      if (entry.stat().filetype != wasi.FILETYPE_DIRECTORY) {
+        return { ret: wasi.ERRNO_NOTDIR, entry: null };
+      }
+    }
+
     return { ret: wasi.ERRNO_SUCCESS, entry };
   }
 
@@ -653,7 +665,6 @@ export class Directory extends Inode {
     entry: Inode | null;
   } {
     let filename = path.parts.pop();
-    path.is_dir = true;
 
     if (filename === undefined) {
       return {
@@ -695,6 +706,18 @@ export class Directory extends Inode {
         return { ret: wasi.ERRNO_SUCCESS, parent_entry, filename, entry: null };
       }
     }
+
+    if (path.is_dir) {
+      if (entry.stat().filetype != wasi.FILETYPE_DIRECTORY) {
+        return {
+          ret: wasi.ERRNO_NOTDIR,
+          parent_entry: null,
+          filename: null,
+          entry: null,
+        };
+      }
+    }
+
     return { ret: wasi.ERRNO_SUCCESS, parent_entry, filename, entry };
   }
 
