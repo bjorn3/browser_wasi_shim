a510ef91ac907a5e53204e06d1af098e77c8aba8
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-12-24 20:39:54 +0100

    Rename fs_core.ts to fs_mem.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export {
   OpenDirectory,
   PreopenDirectory,
   ConsoleStdout,
-} from "./fs_core.js";
+} from "./fs_mem.js";
 export { SyncOPFSFile, OpenSyncOPFSFile } from "./fs_opfs.js";
 export { strace } from "./strace.js";
 export * as wasi from "./wasi_defs.js";
