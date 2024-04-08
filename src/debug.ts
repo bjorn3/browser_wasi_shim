1753c2d92ce09605a0eeb7e62640cd6e1665c84f
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-04-08 19:54:56 +0200

    Fix formatting for debug.ts

--- src/debug.ts
+++ src/debug.ts
@@ -22,7 +22,10 @@ class Debug {
 
 // The createLogger() creates either an empty function or a bound console.log
 // function so we can retain accurate line lumbers on Debug.log() calls.
-function createLogger(enabled: boolean, prefix?: string): (...args: unknown[]) => void {
+function createLogger(
+  enabled: boolean,
+  prefix?: string,
+): (...args: unknown[]) => void {
   if (enabled) {
     const a = console.log.bind(console, "%c%s", "color: #265BA0", prefix);
     return a;
